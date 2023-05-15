08dc700281f51e42c0648e979595235498b3fbf6
elderguardian <soydev@riseup.net>
2023-05-15 20:58:54 +0200

    added check if the show exists when removing one

--- src/commands/anime/airing/operations/removeOperation.js
+++ src/commands/anime/airing/operations/removeOperation.js
@@ -1,8 +1,21 @@
-const {removeShow} = require("../../../../foundations/mongo/guildManager")
+const {removeShow, showExists} = require("../../../../foundations/mongo/guildManager")
 
-module.exports = (client, message, show) => {
+module.exports = async (client, message, show) => {
     const mongoDb = client.db
     const guildId = message.guild.id
+
+    try {
+        const isValidShow = await showExists(mongoDb, guildId, show)
+
+        if (!isValidShow) {
+            message.reply('This show has not been added yet.')
+            return
+        }
+
+    } catch (err) {
+        message.reply(`There was an error validating the existence of your show: ${err.message}`)
+        return
+    }
 
     removeShow(mongoDb, guildId, show).then(() => {
         message.channel.send(`Removed show \`${show}\` from the list.`)
