834eae7feabe151d3653cbca53ed64ee746b5bd4
elderguardian <katzenmaedchen@tutanota.com>
2023-05-27 15:59:54 +0200

    fixed bug with air command permissions

--- src/commands/anime/airing/index.js
+++ src/commands/anime/airing/index.js
@@ -18,7 +18,7 @@ module.exports = {
     'usage': '<operation>',
     'filter': {
         'sender': [
-            //PermissionsBitField.Flags.Administrator
+            PermissionsBitField.Flags.Administrator
         ],
     },
     'callback': (client, message, [operation, ...showSplit]) => {
