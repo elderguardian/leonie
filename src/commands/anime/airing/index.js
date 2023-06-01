a24cdb4c71abe4031916f9d11fd953072ec898c2
elderguardian <katzenmaedchen@tutanota.com>
2023-06-01 20:24:18 +0200

    forgot to comment line back in

--- src/commands/anime/airing/index.js
+++ src/commands/anime/airing/index.js
@@ -20,7 +20,7 @@ module.exports = {
     'usage': '<operation>',
     'filter': {
         'sender': [
-            //PermissionsBitField.Flags.Administrator
+            PermissionsBitField.Flags.Administrator
         ],
     },
     'callback': (client, message, [operation, ...showSplit]) => {
