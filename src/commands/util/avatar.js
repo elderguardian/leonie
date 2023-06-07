a6099bde4a84fefb3f5fef7503008571d6548ca8
elderguardian <katzenmaedchen@tutanota.com>
2023-06-07 20:12:38 +0200

    implemented avatar command

---
+++ src/commands/util/avatar.js
@@ -0,0 +1,18 @@
+const sendImageEmbed = require('../../foundations/embed/sendImageEmbed')
+
+module.exports = {
+    'callback': (client, message, arguments) => {
+
+        let membersToSend = message.mentions.members.size > 1
+            ? [...message.mentions.members.map(g => g.user)]
+            : [message.author]
+
+        for (const member of membersToSend) {
+            const username = member.username
+            const avatarUrl = member.avatarURL()
+
+            sendImageEmbed(message.channel, `Avatar of \`${username}\``, avatarUrl)
+        }
+    }
+}
+
