52de86bfcf181585a30203ab7283554a8af742f1
elderguardian <katzenmaedchen@tutanota.com>
2023-06-30 22:32:55 +0200

    removed old bot files

--- src/foundations/anime/parseTitle.js
+++
@@ -1,7 +0,0 @@
-module.exports = title => title['english']
-    ? title['english']
-    : title['romaji']
-        ? title['romaji']
-        : title['native']
-            ? title['native']
-            : 'Title is unknown'
