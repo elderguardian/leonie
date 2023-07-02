50aaf0ea84c85d6ac050f93d61b01233866c4266
elderguardian <katzenmaedchen@tutanota.com>
2023-07-02 18:32:54 +0200

    implemented avatar command logic

--- src/commands/AvatarCommand.ts
+++ src/commands/AvatarCommand.ts
@@ -3,6 +3,14 @@ import { ICommand } from "../foundations/ICommand";
 import { ICommandRunOptions } from "../foundations/ICommandRunOptions";
 import * as config from "../../config.json";
 
+async function fetchAvatarUrl(
+    target: User,
+    scope: string,
+    guildId: string
+): Promise<string> {
+    return ''
+}
+
 const pingCommand: ICommand = {
   data: new SlashCommandBuilder()
       .addUserOption((option) =>
@@ -24,7 +32,28 @@ const pingCommand: ICommand = {
       runOptions: ICommandRunOptions,
       interaction: CommandInteraction
   ): Promise<void> {
+      await interaction.deferReply();
 
+      const target = interaction.options.getUser("target", true);
+      const scopeObject = interaction.options.get("scope", false);
+
+      const scopeValue = scopeObject ? scopeObject.value : "";
+      const scope = scopeObject && scopeValue ? scopeValue : "global_avatar";
+
+      if (!interaction.guild) {
+          await interaction.editReply("This command is guild only.");
+          return;
+      }
+
+      await fetchAvatarUrl(target, scope.toString(), interaction.guild.id ?? "")
+          .then(async (avatarUrl) => {
+              await interaction.editReply(avatarUrl);
+          })
+          .catch(async (err) => {
+              await interaction.editReply(
+                  `Error while fetching avatar: ${err.message}`
+              );
+          });
   },
 };
 
