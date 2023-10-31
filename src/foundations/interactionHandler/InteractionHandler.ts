2e4207e5464eb6857b4514a328054ae165d1e5f4
elderguardian <katzenmaedchen@tutanota.com>
2023-10-31 17:52:33 +0100

    switched from command objects to exporting classes

--- src/foundations/interactionHandler/InteractionHandler.ts
+++ src/foundations/interactionHandler/InteractionHandler.ts
@@ -1,41 +1,37 @@
 import { CommandInteraction } from "discord.js";
-import { ICommand } from "../ICommand";
-import { ICommandRunOptions } from "../ICommandRunOptions";
+import { ICommand } from "../command/ICommand";
+import { ICommandRunOptions } from "../command/ICommandRunOptions";
 import { IActionLoader } from "../actionLoader/IActionLoader";
 import { IInteractionHandler } from "./IInteractionHandler";
 import process from "process";
 
 export class InteractionHandler implements IInteractionHandler {
-  private commands: ICommand[];
-  private fileLoader: IActionLoader;
+    private commands: ICommand[];
+    private fileLoader: IActionLoader;
 
-  constructor(fileLoader: IActionLoader) {
-    this.fileLoader = fileLoader;
-    this.commands = [];
-    fileLoader
-      .loadCommands(
-        process.env.LEONIE_DEV == "true" ? "src/commands" : "commands"
-      )
-      .then((commands) => (this.commands = commands));
-  }
+    constructor(fileLoader: IActionLoader) {
+        this.fileLoader = fileLoader;
+        this.commands = [];
 
-  handle(
-    runOptions: ICommandRunOptions,
-    interaction: CommandInteraction
-  ): void {
-    if (!interaction.isChatInputCommand()) {
-      return;
+        fileLoader
+            .loadCommands(process.env.LEONIE_DEV == "true" ? "src/commands" : "commands")
+            .then((commands) => (this.commands = commands));
     }
 
-    const { commandName } = interaction;
-    const commandInstance = this.commands.filter(
-      (command: ICommand) => command.data.name === commandName
-    )[0];
+    handle(runOptions: ICommandRunOptions, interaction: CommandInteraction): void {
+        if (!interaction.isChatInputCommand()) {
+            return;
+        }
 
-    if (!commandInstance) {
-      throw new Error("This command does not exists.");
+        const { commandName } = interaction;
+        const commandInstance = this.commands.filter(
+            (command: ICommand) => command.getMetadata().name === commandName
+        )[0];
+
+        if (!commandInstance) {
+            throw new Error("This command does not exists.");
+        }
+
+        commandInstance.run(runOptions, interaction);
     }
-
-    commandInstance.run(runOptions, interaction);
-  }
 }
