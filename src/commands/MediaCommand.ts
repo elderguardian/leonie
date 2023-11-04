9063c32818aaeaa6133903a21f4b9540b66507e4
elderguardian <katzenmaedchen@tutanota.com>
2023-11-04 13:58:07 +0100

    implemented anime search

--- src/commands/MediaCommand.ts
+++ src/commands/MediaCommand.ts
@@ -1,5 +1,8 @@
-import { SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { ICommand } from "../foundations/command/ICommand";
+import { ICommandRunOptions } from "../foundations/command/ICommandRunOptions";
+import { leonieConfig } from "../core/config/LeonieConfig";
+import { kernel } from "../core/ioc/Container";
 
 export class MediaCommand implements ICommand {
     getMetadata(): SlashCommandBuilder {
@@ -16,6 +19,96 @@ export class MediaCommand implements ICommand {
                     { name: "Character", value: "character"}))
             .setName("media")
             .setDescription("Get metadata about different media");
+    }
+
+    async run(runOptions: ICommandRunOptions, interaction: CommandInteraction): Promise<void> {
+        await interaction.deferReply();
+
+        const mediaTitleOption = interaction.options.get("title", true);
+        const mediaTypeOptions = interaction.options.get("type", false);
+
+        const typeAsString = mediaTypeOptions?.value || "anime";
+        const mediaTitle = String(mediaTitleOption.value);
+
+        try {
+            await this.handleAnimeOption(interaction, mediaTitle)
+        } catch (error: any) {
+            await interaction.editReply({
+                content: `Failed executing: ${error.message}`
+            })
+        }
+    }
+
+    private async handleAnimeOption(interaction: CommandInteraction, mediaTitle: string): Promise<void> {
+        const animeFetcher = kernel.get("IAnimeFetcher");
+        const metadata = await animeFetcher.fetchAnime(mediaTitle);
+
+        const displayedEndDate = metadata.endDate
+            ? this.formatDateToShortDate(metadata.endDate)
+            : "Not available";
+
+        const displayedStartDate = this.formatDateToShortDate(metadata.startDate);
+
+        let embedDescription = `## ${metadata.title}\n` +
+            `\`${metadata.genres.join(", ")}\`\n` +
+            `### \`${displayedStartDate}\` to \`${displayedEndDate}\`\n` +
+            `\nEpisodes: \`${metadata.episodes.amount}\`\n` +
+            `Episode Duration: \`${metadata.episodes.duration}m\`\n`;
+
+        if (metadata.nextAiringEpisode) {
+            embedDescription +=
+                `\nNext Episode: \`${metadata.nextAiringEpisode.episode}\`\n` +
+                `Airing at: \`${metadata.episodes.airingAt.toISOString()}\`\n` +
+                `Airing in: \`${this.formatDeltaTime(metadata.episodes.timeUntilAiring)}\`\n`;
+        }
+
+        const metadataEmbed = this.buildAniListEmbed()
+            .setURL(metadata.siteUrl)
+            .setDescription(embedDescription)
+            .setThumbnail(metadata.images.cover)
+            .setImage(metadata.images.banner);
+
+        await interaction.editReply({ embeds: [metadataEmbed], });
+    }
+
+    private buildAniListEmbed() : EmbedBuilder {
+        return new EmbedBuilder()
+            .setColor(leonieConfig.embed_color)
+            .setTitle("AniList Page")
+            .setAuthor({
+                name: "AniList Search",
+                url: "https://anilist.co/",
+                iconURL: "https://anilist.co/img/icons/favicon-32x32.png",
+            })
+            .setFooter({
+                text: "If this is not what you wanted, complain to AniList",
+            });
+    }
+
+    private formatDateToShortDate(date: Date): string {
+        const options: Intl.DateTimeFormatOptions = {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+        };
+        return date.toLocaleDateString(undefined, options);
+    }
+
+    private formatDeltaTime(deltaTime: number) {
+        let deltaRest = deltaTime;
+
+        const days = Math.floor(deltaRest / 86400);
+        deltaRest -= days * 86400;
+
+        const hours = Math.floor(deltaRest / 3600) % 24;
+        deltaRest -= hours * 3600;
+
+        const minutes = Math.floor(deltaRest / 60) % 60;
+        deltaRest -= minutes * 60;
+
+        const seconds = deltaRest % 60;
+
+        return `${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s`;
     }
 
 }
