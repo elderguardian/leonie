b61a8414cbdeceb5efafc2ffa4f955571ce409a5
elderguardian <katzenmaedchen@tutanota.com>
2023-11-25 10:38:09 +0100

    add github mapping to container

--- src/core/ioc/Container.ts
+++ src/core/ioc/Container.ts
@@ -3,8 +3,10 @@ import { Kernel } from "./Kernel";
 import { DiscordFetcher } from "../../components/discordFetcher/DiscordFetcher";
 import { AnimeFetcher } from "../../components/animeFetcher/AnimeFetcher";
 import { WeatherFetcher } from "../../components/weatherFetcher/WeatherFetcher";
+import { GithubFetcher } from "../../components/githubFetcher/GithubFetcher";
 
 export const kernel: IKernel = new Kernel()
     .set("IDiscordFetcher", () => new DiscordFetcher())
     .set("IAnimeFetcher", () => new AnimeFetcher())
     .set("IWeatherFetcher", () => new WeatherFetcher())
+    .set("IGitHubFetcher", () => new GithubFetcher());
