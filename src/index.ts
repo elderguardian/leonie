345d909ad12531ff44110814ecefb3e1630ff8e7
elderguardian <katzenmaedchen@tutanota.com>
2024-01-13 17:22:20 +0100

    fix commands always reloading

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { Client, CommandInteraction, Events, IntentsBitField } from "discord.js";
 import { InteractionHandler } from "./foundations/interactionHandler/InteractionHandler";
-import { ActionLoader } from "./foundations/actionLoader/ActionLoader";
 import * as dotenv from "dotenv";
 import * as process from "process";
+import { ICommand } from "./foundations/command/ICommand";
+import { ActionLoader } from "./foundations/actionLoader/ActionLoader";
 
 dotenv.config();
 
-const fileLoader = new ActionLoader();
-const interactionHandler = new InteractionHandler(fileLoader);
+const interactionHandler = new InteractionHandler();
+
+let commands: ICommand[] | undefined;
 
 const client = new Client({
     intents: [
@@ -27,9 +29,15 @@ client.once(Events.ClientReady, (client: Client) => {
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-    interactionHandler.handle({ client }, <CommandInteraction>interaction);
+    if (!commands) {
+        return;
+    }
+
+    interactionHandler.handle({ client, commands }, <CommandInteraction>interaction);
 });
 
-client.login(process.env.LEONIE_BOT_TOKEN).then((r) => {
+client.login(process.env.LEONIE_BOT_TOKEN).then(async (r) => {
+    const commandDir = process.env.LEONIE_DEV == "true" ? "src/commands" : "commands";
+    commands = await (new ActionLoader()).loadCommands(commandDir);
     console.log("Logging into Discord...");
 });
