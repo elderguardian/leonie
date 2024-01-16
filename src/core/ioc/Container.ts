657f6eb0bf6372bfc7148aa14237341b7deb74d2
elderguardian <katzenmaedchen@tutanota.com>
2024-01-16 18:50:40 +0100

    add interaction handler to di container

--- src/core/ioc/Container.ts
+++ src/core/ioc/Container.ts
@@ -4,8 +4,10 @@ import { DiscordFetcher } from "../../components/discordFetcher/DiscordFetcher";
 import { AnimeFetcher } from "../../components/animeFetcher/AnimeFetcher";
 import { WeatherFetcher } from "../../components/weatherFetcher/WeatherFetcher";
 import { GithubFetcher } from "../../components/githubFetcher/GithubFetcher";
+import { InteractionHandler } from "../../foundations/interactionHandler/InteractionHandler";
 
 export const kernel: IKernel = new Kernel()
+    .set('IInteractionHandler', () => new InteractionHandler())
     .set("IDiscordFetcher", () => new DiscordFetcher())
     .set("IAnimeFetcher", () => new AnimeFetcher())
     .set("IWeatherFetcher", () => new WeatherFetcher())
