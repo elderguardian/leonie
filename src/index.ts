9bf6ba9c3ba42158c37be33d89d8df69bbf32bd1
elderguardian <katzenmaedchen@tutanota.com>
2024-01-16 18:59:42 +0100

    clean up mess

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,46 @@
 import { Client, CommandInteraction, Events, IntentsBitField } from "discord.js";
-import { InteractionHandler } from "./foundations/interactionHandler/InteractionHandler";
 import * as dotenv from "dotenv";
 import * as process from "process";
-import { ICommand } from "./foundations/command/ICommand";
 import { ActionLoader } from "./foundations/actionLoader/ActionLoader";
+import { kernel } from "./core/ioc/Container";
 
 dotenv.config();
 
-const interactionHandler = new InteractionHandler();
+new (class {
 
-let commands: ICommand[] | undefined;
-
-const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ],
-});
-
-client.once(Events.ClientReady, (client: Client) => {
-    if (!client.user) {
-        return;
+    private createClient() {
+        return new Client({
+            intents: [
+                IntentsBitField.Flags.Guilds,
+                IntentsBitField.Flags.GuildMembers,
+                IntentsBitField.Flags.GuildMessages,
+                IntentsBitField.Flags.MessageContent,
+            ],
+        });
     }
 
-    console.log(`Logged in as user: ${client.user.username ?? "unknown"}`);
-});
+    public async initialize() {
+        const client = this.createClient();
 
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (!commands) {
-        return;
+        const commandDir = process.env.LEONIE_DEV == "true" ? "src/commands" : "commands";
+        const commands = await (new ActionLoader()).loadCommands(commandDir);
+
+        const interactionHandler = kernel.get("IInteractionHandler");
+
+        const token = process.env.LEONIE_BOT_TOKEN;
+
+        client.once(Events.ClientReady, client => {
+            if (!client.user) return;
+            console.log(`Logged in as user: ${client.user.username ?? "unknown"}`);
+        });
+
+        client.on(Events.InteractionCreate, async interaction => {
+            if (!commands) return;
+            interactionHandler.handle({ client, commands }, <CommandInteraction>interaction);
+        })
+
+        await client.login(token)
+        console.log("Logging into Discord...");
     }
 
-    interactionHandler.handle({ client, commands }, <CommandInteraction>interaction);
-});
-
-client.login(process.env.LEONIE_BOT_TOKEN).then(async (r) => {
-    const commandDir = process.env.LEONIE_DEV == "true" ? "src/commands" : "commands";
-    commands = await (new ActionLoader()).loadCommands(commandDir);
-    console.log("Logging into Discord...");
-});
+})().initialize().then(r => console.log("Started bot successfully."));
