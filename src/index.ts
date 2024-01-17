ba1150b1cd1fee4dc53a6f6758a004b32227ac07
elderguardian <katzenmaedchen@tutanota.com>
2024-01-17 17:36:38 +0100

    start bot in index

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,12 @@
-import { Client, CommandInteraction, Events, IntentsBitField } from "discord.js";
 import * as dotenv from "dotenv";
-import * as process from "process";
-import { ActionLoader } from "./foundations/actionLoader/ActionLoader";
-import { kernel } from "./core/ioc/Container";
+import { Bot } from "./core/bot/Bot";
 
 dotenv.config();
 
-new (class {
+async function startBot() {
+    const bot = new Bot();
+    await bot.initialize();
+    console.log("Started bot successfully.");
+}
 
-    private createClient() {
-        return new Client({
-            intents: [
-                IntentsBitField.Flags.Guilds,
-                IntentsBitField.Flags.GuildMembers,
-                IntentsBitField.Flags.GuildMessages,
-                IntentsBitField.Flags.MessageContent,
-            ],
-        });
-    }
-
-    public async initialize() {
-        const client = this.createClient();
-
-        const commandDir = process.env.LEONIE_DEV == "true" ? "src/commands" : "commands";
-        const commands = await (new ActionLoader()).loadCommands(commandDir);
-
-        const interactionHandler = kernel.get("IInteractionHandler");
-
-        const token = process.env.LEONIE_BOT_TOKEN;
-
-        client.once(Events.ClientReady, client => {
-            if (!client.user) return;
-            console.log(`Logged in as user: ${client.user.username ?? "unknown"}`);
-        });
-
-        client.on(Events.InteractionCreate, async interaction => {
-            if (!commands) return;
-            interactionHandler.handle({ client, commands }, <CommandInteraction>interaction);
-        })
-
-        await client.login(token)
-        console.log("Logging into Discord...");
-    }
-
-})().initialize().then(r => console.log("Started bot successfully."));
+startBot();
