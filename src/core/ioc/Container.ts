8057f95f5551a5c67ecf3329cec09331b056ec69
elderguardian <katzenmaedchen@tutanota.com>
2024-01-19 15:08:08 +0100

    register luther insulter di mapping

--- src/core/ioc/Container.ts
+++ src/core/ioc/Container.ts
@@ -6,6 +6,7 @@ import { WeatherFetcher } from "../../components/weatherFetcher/WeatherFetcher";
 import { GithubFetcher } from "../../components/githubFetcher/GithubFetcher";
 import { InteractionHandler } from "../../foundations/interactionHandler/InteractionHandler";
 import { ActionLoader } from "../../foundations/actionLoader/ActionLoader";
+import { LutherInsulter } from "../../components/lutherInsulter/LutherInsulter";
 
 export const kernel: IKernel = new Kernel()
     .set("IInteractionHandler", () => new InteractionHandler())
@@ -13,4 +14,5 @@ export const kernel: IKernel = new Kernel()
     .set("IDiscordFetcher", () => new DiscordFetcher())
     .set("IAnimeFetcher", () => new AnimeFetcher())
     .set("IWeatherFetcher", () => new WeatherFetcher())
-    .set("IGitHubFetcher", () => new GithubFetcher());
+    .set("IGitHubFetcher", () => new GithubFetcher())
+    .set("ILutherInsulter", () => new LutherInsulter());
