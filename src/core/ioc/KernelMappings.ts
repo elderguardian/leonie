f3f9d4afa8c6fdbe607a0b89dfac0618d4d241ac
elderguardian <katzenmaedchen@tutanota.com>
2024-01-26 20:33:20 +0100

    create enum for new di container usage

---
+++ src/core/ioc/KernelMappings.ts
@@ -0,0 +1,9 @@
+export enum KernelMappings {
+    INTERACTION_HANDLER = "IInteractionHandler",
+    ACTION_LOADER = "IActionLoader",
+    DISCORD_FETCHER = "IDiscordFetcher",
+    ANIME_FETCHER = "IAnimeFetcher",
+    WEATHER_FETCHER = "IWeatherFetcher",
+    GITHUB_FETCHER = "IGitHubFetcher",
+    LUTHER_INSULTER = "ILutherInsulter",
+}
