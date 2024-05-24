db11143bb91065f0f87a01b0ecc6d8538e3706fb
elderguardian <katzenmaedchen@tutanota.com>
2024-05-24 12:19:49 +0100

    extend cocktail data
    extend cocktail data and implement cache

--- src/components/cocktailFetcher/contract/ICocktail.ts
+++ src/components/cocktailFetcher/contract/ICocktail.ts
@@ -2,4 +2,6 @@ export interface ICocktail {
     ingredients: string[];
     instructions: string;
     name: string;
+    category: string;
+    thumbnail: string;
 }
--- src/components/cocktailFetcher/impl/CocktailFetcher.ts
+++ src/components/cocktailFetcher/impl/CocktailFetcher.ts
@@ -3,20 +3,55 @@ import { ICocktail } from "../contract/ICocktail";
 
 export class CocktailFetcher implements ICocktailFetcher {
 
-    async fetchCocktails(name: string): Promise<ICocktail> {
-        const response = await fetch("https://api.api-ninjas.com/v1/cocktail?name=" + name);
+    private cocktails: Map<string, ICocktail>;
 
-        if (!response.ok) {
-            throw new Error("Failed to fetch cocktail.");
-        }
-
-        const data: any = await response.json();
-
-        return {
-            ingredients: data.ingredients ?? [],
-            instructions: data.instructions ?? "No instructions available.",
-            name: data.name ?? "No name available."
-        };
+    constructor() {
+        this.cocktails = new Map<string, ICocktail>();
     }
 
+    async fetchCocktails(name: string): Promise<ICocktail> {
+
+        if (!this.cocktails) {
+            this.cocktails = new Map<string, ICocktail>();
+        }
+
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+        const data: any = await response.json();
+
+        if (!data.drinks) {
+            throw new Error("No drinks found.");
+        }
+
+        const drink = data.drinks[0];
+        const ingredients = this.getIngredients(drink);
+        const instructions = drink.strInstructions;
+
+        const cocktail = {
+            name: drink.strDrink,
+            ingredients,
+            instructions,
+            category: drink.strCategory,
+            thumbnail: drink.strDrinkThumb
+        };
+
+        this.cocktails.set(name, cocktail);
+        return cocktail;
+    }
+
+    private getIngredients(drink: any) {
+        const ingredients = [];
+
+        for (let i = 1; i <= 15; i++) {
+            const ingredient = drink[`strIngredient${i}`];
+            const measure = drink[`strMeasure${i}`];
+
+            if (!ingredient) {
+                break;
+            }
+
+            ingredients.push(`${ingredient} ${measure}`);
+        }
+
+        return ingredients;
+    }
 }
